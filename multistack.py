73276fe59d2a03024bef10da54212b5dbafa4c25
AndrewSB <asbreckenridge@me.com>
2014-11-25 09:05:00 -0500

    create basic onAdd checks

--- multistack.py
+++ multistack.py
@@ -4,6 +4,10 @@ class MultiStack:
 	def __init__(self):
 		print "multi stack created"
 		self.mainArray = []
+		self.matchesArray = []
+
+	def addStack(self, name):
+		self.mainArray.append([name])
 
 	def addEntry(self, e):
 		i = 0
@@ -12,20 +16,23 @@ class MultiStack:
 			if e.name == entryArray[0]:
 				self.mainArray[i].append(e)
 				added = True
+				self.checkStacksForInteraction(e)
 				return self.mainArray[i][-1]
 			i+=1
 		i = 0
 		if not added:
 			self.addStack(e.name)
 			self.mainArray[-1].append(e)
+			self.checkStacksForInteraction(e)
 			return self.mainArray[-1][-1]
 		return False
 
-
-
-	def addStack(self, name):
-		self.mainArray.append([name])
-
+	def checkStacksForInteraction(self, e):
+		print "check "+e.stringify()
+		for entryArray in self.mainArray:
+			if not e.name == entryArray[0]:
+				if e.isInRange(entryArray[-1]):
+					print "hit with " + entryArray[-1].stringify()
 
 	def stringify(self):
 		for entryArray in self.mainArray:
@@ -36,12 +43,9 @@ class MultiStack:
 					print entry
 
 
-# ms = MultiStack()
-# ms.addEntry(entry.Entry("unclejoey|1327430121|37.777372247259|-122.3981163202"))
-# ms.addEntry(entry.Entry("jj|1327437628|37.777253789662|-122.3981738187"))
-# ms.addEntry(entry.Entry("danny|1327428557|37.77723932161|-122.39818298999"))
-# ms.addEntry(entry.Entry("danny|1327428557|37.77723932161|-122.39818298999"))
-# ms.addEntry(entry.Entry("unclejoey|1327430121|37.777372247259|-122.3981163202"))
-
-
-# ms.stringify()
+ms = MultiStack()
+ms.addEntry(entry.Entry("unclejoey|1327430121|37.777372247259|-122.3981163202"))
+ms.addEntry(entry.Entry("jj|1327437628|37.777253789662|-122.3981738187"))
+ms.addEntry(entry.Entry("danny|1327428557|37.77723932161|-122.39818298999"))
+ms.addEntry(entry.Entry("danny|1327428557|37.77723932161|-122.39818298999"))
+ms.addEntry(entry.Entry("unclejoey|1327430121|37.777372247259|-122.3981163202"))
