16bc4e2be7faaf0c71ed335b8aac532598500a9e
AndrewSB <asbreckenridge@me.com>
2014-11-25 11:24:44 -0500

    add results and cleanup console output

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ for line in open("userdata.txt", 'r'):
 	if not ms.addEntry(entry.Entry(line)):
 		print("ERROR: could not check" + line + " for some reason.")
 
-results = ms.matchesArray
+print("\n\n\n\nFINAL OUTPUT___________________________________________________________________")
+
 writeFile = open("results.txt", 'w')
-for result in results:
+for result in ms.matchesArray:
 	print(result.stringify())
 	print(result.csvify(), file=writeFile)
